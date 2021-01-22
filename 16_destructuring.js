197d3798dd998be1b05ccca6a98a60a0df28b241
Yevhen <yevhenlebid51@gmail.com>
2021-01-22 13:00:10 +0200

    16_destructuring.js

--- 16_destructuring.js
+++ 16_destructuring.js
@@ -1,10 +1,40 @@
 function calculateValues(a, b) {
     return [
         a + b,
-        a - b
+        a- b,
+        a * b,
+        a / b
     ]
 }
 
-const [sum, sub] = calculateValues(42, 10)
+// const [sum, sub = 'Вычитания нет', mult, ...other] = calculateValues(42, 10)
+//
+// console.log(sum, mult, other, sub)
 
-console.log(sum, sub)
+// Objects
+const person = {
+    name: 'Max',
+    age: 20,
+    address: {
+        country: 'Ukraine',
+        city: 'Kyiv'
+    }
+}
+
+// const {
+//     name: firstName = 'Без имени',
+//     age,
+//     car = 'Машины нет',
+//     address: {country, city: homeTown},
+// } = person
+
+const {name, ...info} = person
+
+// console.log(firstName, age, car, country, homeTown)
+// console.log(name, info)
+
+function logPerson({name: firstName = 'Имя не указано', age}) {
+    console.log(name + ' ' + age)
+}
+
+logPerson(person)
